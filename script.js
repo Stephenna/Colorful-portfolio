c0c48f04dc0a20b8f949ccac4dc35f36cf913e3f
Stephenna J Corry <Sjcstephenna@gmail.com>
2020-11-16 21:43:15 -0800

    mostly complete, just have to add projects

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ const colorArr = [
 
 
 // EVENTS
-    document.addEventListener('click', shapes);
-    function shapes(){
-        const newDiv = document.createElement('div');
-        newDiv.classList.add("jsShapeContainer");
-        document.body.appendChild(newDiv);
-        const shaped = document.createElement('div');
-        shaped.classList.add('jsShape');
-        newDiv.appendChild(shaped);
+    // document.addEventListener('click', shapes);
+    // function shapes(){
+    //     const newDiv = document.createElement('div');
+    //     newDiv.classList.add("jsShapeContainer");
+    //     document.body.appendChild(newDiv);
+    //     const shaped = document.createElement('div');
+    //     shaped.classList.add('jsShape');
+    //     newDiv.appendChild(shaped);
 
-        console.log(newDiv)
-    }
-    shapes()
+    //     console.log(newDiv)
+    // }
+    // shapes()
  
 // FUNCTIONS
     function colorfulName(){
@@ -53,4 +53,53 @@ const colorArr = [
 
 // CALLS
 colorfulName();
+
+
+
+// Some random colors
+const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+
+const numBalls = 100;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 470)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+  
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    ],
+    {
+      duration: (Math.random() + 1) * 2000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  );
+});
+
+
+
+
+
 
