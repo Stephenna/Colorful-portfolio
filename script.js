f3f60577545ab83c3a72b8055f4a7f5d66427a08
Stephenna J Corry <Sjcstephenna@gmail.com>
2020-11-23 22:47:13 -0800

    pls wrk

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ for (let i = 0; i < numBalls; i++) {
   ball.classList.add("ball");
   ball.style.background = colors[Math.floor(Math.random() * colors.length)];
   ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-  ball.style.top = `${Math.floor(Math.random() * 470)}vh`;
+  ball.style.top = `${Math.floor(Math.random() * 300)}vh`;
   ball.style.transform = `scale(${Math.random()})`;
   ball.style.width = `${Math.random()}em`;
   ball.style.height = ball.style.width;
